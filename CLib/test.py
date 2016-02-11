c6bec494026b2b135844c10b92f6a70e7b5c617a
Kenji Ogura <k5iogura.v40@gmail.com>
2016-02-11 17:41:44 +0000

    revise

--- CLib/test.py
+++ CLib/test.py
@@ -33,66 +33,6 @@ colors = [(254.0, 254.0, 254), (239.8, 211.6, 127),
 # YOLOv2 anchor of Bounding-Boxes
 anchors = [1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52]
 
-def sigmoid(x):
-  return 1. / (1. + np.exp(-x))
-
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    out = e_x / e_x.sum()
-    return out
-
-def iou(boxA,boxB):
-  # boxA = boxB = [x1,y1,x2,y2]
-
-  xA = max(boxA[0], boxB[0])
-  yA = max(boxA[1], boxB[1])
-  xB = min(boxA[2], boxB[2])
-  yB = min(boxA[3], boxB[3])
- 
-  # Compute the area of intersection
-  intersection_area = (xB - xA + 1) * (yB - yA + 1)
- 
-  # Compute the area of both rectangles
-  boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-  boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
- 
-  # Compute the IOU
-  iou = intersection_area / float(boxA_area + boxB_area - intersection_area)
-
-  return iou
-
-
-
-def non_maximal_suppression(thresholded_predictions,iou_threshold):
-
-  nms_predictions = []
-
-  # Add the best B-Box because it will never be deleted
-  if len(thresholded_predictions)<=0: return nms_predictions
-  nms_predictions.append(thresholded_predictions[0])
-
-  # For each B-Box (starting from the 2nd) check its iou with the higher score B-Boxes
-  # thresholded_predictions[i][0] = [x1,y1,x2,y2]
-  i = 1
-  while i < len(thresholded_predictions):
-    n_boxes_to_check = len(nms_predictions)
-    to_delete = False
-
-    j = 0
-    while j < n_boxes_to_check:
-        curr_iou = iou(thresholded_predictions[i][0],nms_predictions[j][0])
-        if(curr_iou > iou_threshold ):
-            to_delete = True
-        j = j+1
-
-    if to_delete == False:
-        nms_predictions.append(thresholded_predictions[i])
-    i = i+1
-
-  return nms_predictions
-
-
-
 def preprocessing(input_img,ph_height,ph_width):
 
   input_image = input_img                           # HWC BGR
@@ -112,87 +52,6 @@ def preprocessing(input_img,ph_height,ph_width):
   #return input_image
   return image_nchwRGB
 
-
-
-def postprocessing(predictions,input_img_path,score_threshold,iou_threshold,ph_height,ph_width):
-
-  input_image = cv2.imread(input_img_path)  # HWC
-  input_image = cv2.resize(input_image,(ph_width, ph_height), interpolation = cv2.INTER_CUBIC)
-
-  thresholded_predictions = []
-  print('Thresholding on (Objectness score)*(Best class score) with threshold = {}'.format(score_threshold))
-
-  predictions = np.reshape(predictions,(grid_h, grid_w, n_b_boxes, n_info_per_grid))
-
-  for row in range(grid_h):
-    for col in range(grid_w):
-      for b in range(n_b_boxes):
-
-        tx, ty, tw, th, tc = predictions[row, col, b, :5]
-
-        center_x = (float(col) + sigmoid(tx)) * 32.0
-        center_y = (float(row) + sigmoid(ty)) * 32.0
-
-        roi_w = np.exp(tw) * anchors[2*b + 0] * 32.0
-        roi_h = np.exp(th) * anchors[2*b + 1] * 32.0
-
-        final_confidence = sigmoid(tc)
-
-        class_predictions = predictions[row, col, b, 5:]
-        class_predictions = softmax(class_predictions)
-
-        class_predictions = tuple(class_predictions)
-        best_class = class_predictions.index(max(class_predictions))
-        best_class_score = class_predictions[best_class]
-
-        left   = int(center_x - (roi_w/2.))
-        right  = int(center_x + (roi_w/2.))
-        top    = int(center_y - (roi_h/2.))
-        bottom = int(center_y + (roi_h/2.))
-        
-        if( (final_confidence * best_class_score) > score_threshold):
-          thresholded_predictions.append([[left,top,right,bottom],final_confidence * best_class_score,classes[best_class]])
-
-  # Sort the B-boxes by their final score
-  thresholded_predictions.sort(key=lambda tup: tup[1],reverse=True)
-
-  print('Printing {} B-boxes survived after score thresholding:'.format(len(thresholded_predictions)))
-  for i in range(len(thresholded_predictions)):
-    print('B-Box {} : {}'.format(i+1,thresholded_predictions[i]))
-
-  # Non maximal suppression
-  print('Non maximal suppression with iou threshold = {}'.format(iou_threshold))
-  nms_predictions = non_maximal_suppression(thresholded_predictions,iou_threshold)
-
-  # Print survived b-boxes
-  print('Printing the {} B-Boxes survived after non maximal suppression:'.format(len(nms_predictions)))
-  for i in range(len(nms_predictions)):
-    print('B-Box {} : {}'.format(i+1,nms_predictions[i]))
-
-  # Draw final B-Boxes and label on input image
-  for i in range(len(nms_predictions)):
-
-      color = colors[classes.index(nms_predictions[i][2])]
-      best_class_name = nms_predictions[i][2]
-
-      # Put a class rectangle with B-Box coordinates and a class label on the image
-      input_image = cv2.rectangle(
-        input_image,
-        ( nms_predictions[i][0][0], nms_predictions[i][0][1] ),
-        ( nms_predictions[i][0][2], nms_predictions[i][0][3] ),
-        color
-      )
-      cv2.putText(
-        input_image,
-        best_class_name,
-        (
-         int((nms_predictions[i][0][0]+nms_predictions[i][0][2])/2),
-         int((nms_predictions[i][0][1]+nms_predictions[i][0][3])/2)
-        ),
-        cv2.FONT_HERSHEY_SIMPLEX,1,color,2)
-  
-  return input_image
-
 def box2rect(box):
     x, y, h, w = box
     lx, ly, rx, ry = x-w/2., y-h/2., x+w/2., y+h/2.
@@ -203,7 +62,6 @@ def box2rect(box):
 def main():
 
 	# Definition of the paths
-#    weights_path      = './yolov2-tiny-voc_352_288_final.weights'
     input_img_path    = './dog.jpg'
     input_img_path    = './horses.jpg'
     input_img_path    = './person.jpg'
