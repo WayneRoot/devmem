01e6d6fd454051f583d9706259da9816f71c17e0
Kenji Ogura <k5i.ogura.v40@gmail.com>
2018-08-19 21:54:57 +0000

    revise

--- python/yv2_parse.py
+++ python/yv2_parse.py
@@ -33,6 +33,7 @@ last_out = (n_classes + 5) * n_boxes
 #yolov2.train = True
 #yolov2.finetune = False
 
+param_adr = 0xc0000000  # param address DDR
 layers=[
     [3, 16, 3], 
     [16, 32, 3], 
@@ -44,7 +45,7 @@ layers=[
     [1024,1024, 1], 
 ]
 
-offset=0
+offset = 0x0
 for i, l in enumerate(layers):
     in_ch, out_ch, ksize = l
     print("[ Layer",i,":", in_ch, out_ch, ksize, ksize, "]")
@@ -58,8 +59,9 @@ for i, l in enumerate(layers):
             bias_buff[o][i] = bias_oCiC[o]
     bias_buff = bias_buff.transpose((1, 0))
     d = bias_buff.tostring()
-    print("write Bytes {:14d} bias {}".format(len(d),bias_buff.shape))
-    devmem(0xe018c000,len(d)).write(d).close()
+    print("0x{:08x} : write Bytes {:14d} bias {}".format(param_adr,len(d),bias_buff.shape))
+    devmem(param_adr,len(d)).write(d).close()
+    param_adr+=len(d)
     offset+=out_ch
 
     # load gamma
@@ -71,7 +73,7 @@ for i, l in enumerate(layers):
             gamma_buff[o][i] = gamma_oCiC[o]
     gamma_buff = gamma_buff.transpose((1, 0))
     d = gamma_buff.tostring()
-    print("write Bytes {:14d} gamma {}".format(len(d),gamma_buff.shape))
+    print("0x{:08x} : write Bytes {:14d} gamma {}".format(param_adr,len(d),gamma_buff.shape))
     offset+=out_ch
 
     # load mean
@@ -83,7 +85,7 @@ for i, l in enumerate(layers):
             mean_buff[o][i] = mean_oCiC[o]
     mean_buff = mean_buff.transpose((1, 0))
     d = mean_buff.tostring()
-    print("write Bytes {:14d} mean {}".format(len(d),mean_buff.shape))
+    print("0x{:08x} : write Bytes {:14d} mean {}".format(param_adr,len(d),mean_buff.shape))
     offset+=out_ch
 
     # load variance
@@ -95,7 +97,7 @@ for i, l in enumerate(layers):
             variance_buff[o][i] = variance_oCiC[o]
     variance_buff = variance_buff.transpose((1, 0))
     d = variance_buff.tostring()
-    print("write Bytes {:14d} variance {}".format(len(d),variance_buff.shape))
+    print("0x{:08x} : write Bytes {:14d} variance {}".format(param_adr,len(d),variance_buff.shape))
     offset+=out_ch
 
     #S = gamma_iCoC / np.sqrt( variance_iCoC )
@@ -106,7 +108,7 @@ for i, l in enumerate(layers):
     weight_oCiCkSkS = dat[offset: offset+out_ch*in_ch*ksize*ksize].reshape((out_ch, in_ch, ksize, ksize))
     weight_iCoCkSkS = weight_oCiCkSkS.transpose((1, 0, 2, 3))
     d = weight_iCoCkSkS.tostring()
-    print("write Bytes {:14d} weight {}".format(len(d),weight_iCoCkSkS.shape))
+    print("0x{:08x} : write Bytes {:14d} weight {}".format(param_adr,len(d),weight_iCoCkSkS.shape))
     offset+= (out_ch*in_ch*ksize*ksize)
 
     #print(i+1, offset)
@@ -125,14 +127,14 @@ for o in range(out_ch):
         bias_buff[o][i] = bias_oCiC[o]
 bias_buff = bias_buff.transpose((1, 0))
 d = bias_buff.tostring()
-print("write Bytes {:14d} bias {}".format(len(d),bias_buff.shape))
+print("0x{:08x} : write Bytes {:14d} bias {}".format(param_adr,len(d),bias_buff.shape))
 offset+=out_ch
 
 # txt = "yolov2.conv%d.W.data = dat[%d:%d].reshape(%d, %d, %d, %d)" % (i+2, offset, offset+(out_ch*in_ch*ksize*ksize), out_ch, in_ch, ksize, ksize)
 weight_oCiCkSkS = dat[offset: offset+out_ch*in_ch*ksize*ksize].reshape((out_ch, in_ch, ksize, ksize))
 weight_iCoCkSkS = weight_oCiCkSkS.transpose((1, 0, 2, 3))   # IOKK
 d = weight_iCoCkSkS.tostring()
-print("write Bytes {:14d} weight {}".format(len(d),weight_iCoCkSkS.shape))
+print("0x{:08x} : write Bytes {:14d} weight {}".format(param_adr,len(d),weight_iCoCkSkS.shape))
 devmem(0xe018c000,len(d)).write(d).close()
 offset+=out_ch*in_ch*ksize*ksize
 
