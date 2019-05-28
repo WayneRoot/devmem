c8c32c6fa1fd63fe7972290c6102a3d9ef010b86
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-05-28 22:13:54 +0900

    revise

--- pygame/sample1.py
+++ pygame/sample1.py
@@ -12,11 +12,11 @@ print "Framebuffer size: %d x %d" % (size[0], size[1])
 #screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 screen = pygame.display.set_mode((size[0]/2,size[1]/2), pygame.RESIZABLE)
 #keifont = pygame.font.Font("keifont.ttf", 80)
-while True:
-    #title = keifont.render(u"なかよしデジカメ", True, (180,0,0))
-    screen.fill((255,230,0))
+for i in range(0,255,15):
+    for j in range(0,255,15):
+        #title = keifont.render(u"なかよしデジカメ", True, (180,0,0))
+        screen.fill((i,j,int((i*j)/255)))
     
-    #screen.blit("abc", (20,150))
-    pygame.display.update()
-    sleep(5)
-    break
+        #screen.blit("abc", (20,150))
+        pygame.display.update()
+        sleep(0.03)
