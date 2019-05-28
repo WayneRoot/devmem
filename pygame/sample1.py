67afd0718d2f445abb8630732f7ebfb0f91aaa9b
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-05-28 21:54:47 +0900

    revise

--- pygame/sample1.py
+++ pygame/sample1.py
@@ -10,7 +10,7 @@ pygame.init()
 size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 print "Framebuffer size: %d x %d" % (size[0], size[1])
 #screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+screen = pygame.display.set_mode((size[0]/2,size[1]/2), pygame.RESIZABLE)
 #keifont = pygame.font.Font("keifont.ttf", 80)
 while True:
     #title = keifont.render(u"なかよしデジカメ", True, (180,0,0))
